cdac5caa22a099e95c4878e0ca5154bfdfb9e235
dlbt96 <42767321+dlbt96@users.noreply.github.com>
2019-12-26 10:21:47 -0500

    Update TweetManager.py

--- GetOldTweets3/manager/TweetManager.py
+++ GetOldTweets3/manager/TweetManager.py
@@ -338,9 +338,9 @@ class TweetManager:
             opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookieJar))
         opener.addheaders = headers
 
-        if debug:
-            print(url)
-            print('\n'.join(h[0]+': '+h[1] for h in headers))
+        #if debug:
+        print(url)
+        print('\n'.join(h[0]+': '+h[1] for h in headers))
         while True:
             try:
                 response = opener.open(url)
